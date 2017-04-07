7679397fe7928fc9d855169a8e007502b3a8162a
deceiver91 <davidgv@openmailbox.org>
2017-04-07 10:08:22 +0200

    Copy all to new proyect WorldEditor

---
+++ WorldEditor/Assets/Resources/Scripts/GameController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+     private GameObject basePanel;
+     private GameObject panelRight;
+     private GameObject panelHide;
+
+     private GameObject mainMenu;
+     private GameObject subMenu;
+     private GameObject[] UIActivables;
+
+     
+     private Stack<GameObject> levels;
+
+     private void Start() {
+          InicializeMenu();
+          
+          levels = new Stack<GameObject>();
+     }
+
+     #region Menus Behavior
+
+     private void InicializeMenu() {
+          basePanel = GameObject.Find("BasePanel");
+          panelRight = GameObject.Find("PanelRight");
+          panelHide = GameObject.Find("PanelHide");
+
+          mainMenu = GameObject.FindGameObjectWithTag("mainMenu");
+          subMenu = GameObject.FindGameObjectWithTag("subMenu");
+          UIActivables = GameObject.FindGameObjectsWithTag("UIActivable");
+
+          basePanel.SetActive(true);
+          panelRight.SetActive(false);
+          panelHide.SetActive(false);
+
+          mainMenu.SetActive(true);
+          subMenu.SetActive(false);          
+     }
+
+     public void MainMenuSetActive(bool isActive) {
+          mainMenu.SetActive(isActive);
+     }
+
+     public void SubMenuSetActive(bool isActive) {
+          subMenu.SetActive(isActive);
+     }
+
+     public void ActiveGameObject(GameObject gameObject) {
+          foreach (var obj in UIActivables) {
+               if (obj.name != gameObject.name) obj.SetActive(false);
+          }
+          gameObject.SetActive(true);
+     }
+
+     public void DisableAllGameObjects() {
+          foreach (var obj in UIActivables) {
+               obj.SetActive(false);
+          }
+     }
+
+     public void BasePanelSetActive(bool isActive) {
+          basePanel.SetActive(isActive);
+     }
+
+     public void PanelRightSetActive(bool isActive) {
+          panelRight.SetActive(isActive);
+     }
+
+     public void PanelHideSetActive(bool isActive) {
+          panelHide.SetActive(isActive);
+     }
+
+     
+     #endregion
+
+
+     #region Menu Levels
+         
+     public void PushGameObject(GameObject gameObject) {
+          levels.Push(gameObject);
+     }
+
+     public GameObject PopGameObject() {
+          return levels.Pop();
+     }
+
+     public int StackCount() {
+          return levels.Count;
+     }
+
+     #endregion
+}
---
+++ WorldEditor/Assets/Resources/Scripts/Menu/buttonsMainMenu.cs
@@ -0,0 +1,155 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonsMainMenu : MonoBehaviour {
+
+     public string Title;
+     public Color textColor;
+     public Color outlineColor;
+     public Texture Image;
+     public GameObject MenuToActive = null;
+
+     private GameController gameController;
+     private Button btn;
+
+     private void Start() {
+          InicializeGameController();
+          InicializeComponents();
+     }
+
+     #region Game Controller
+
+     private void InicializeGameController() {
+          GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+          if (gameControllerObject != null) {
+               gameController = gameControllerObject.GetComponent<GameController>();
+          }
+          if (gameController == null) {
+               Debug.Log("Cannot find 'GameController' script");
+          }
+     }
+
+     #endregion
+
+     #region Config Button
+
+     private void InicializeComponents() {
+          textColor = new Color(textColor.r, textColor.g, textColor.b, 1.0f); // Para que coja bien el color hay que darle el alpha
+          outlineColor = new Color(outlineColor.r, outlineColor.g, outlineColor.b, 1.0f); // Para que coja bien el color hay que darle el alpha
+
+          btn = GetComponent<Button>();
+
+          if (Title != "") UpdateText();
+          else ClearText();
+          UpdateBackground();
+          if (Image != null) UpdateImageBackground();
+
+          btn.onClick.AddListener(TaskOnClick);
+     }
+
+     private void UpdateText() {
+          Text text = btn.GetComponentInChildren<Text>();
+          text.text = string.Format("{0}", Title);
+          text.color = textColor;          
+          Outline line = btn.GetComponentInChildren<Outline>();
+          line.effectColor = outlineColor;          
+     }
+
+     private void ClearText() {
+          Text text = btn.GetComponentInChildren<Text>();
+          text.text = string.Format("");
+     }
+
+     private void UpdateBackground() {
+          Image img = btn.GetComponent<Image>();
+          img.color = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+     }
+
+     private void UpdateImageBackground() {
+          RawImage img = btn.GetComponentInChildren<RawImage>();
+          img.texture = (Texture) Image;
+     }
+
+     #endregion
+
+     #region Extra Functions
+     
+     private void DisableSubMenus() {
+          gameController.MainMenuSetActive(false);
+          gameController.SubMenuSetActive(true);
+          gameController.DisableAllGameObjects();
+     }
+
+     private void DisablePanelRight() {
+          gameController.BasePanelSetActive(true);
+          gameController.PanelRightSetActive(false);
+          gameController.PanelHideSetActive(false);
+     }
+
+     private void ActivePanelRight() {
+          gameController.BasePanelSetActive(false);
+          gameController.PanelRightSetActive(true);
+          gameController.PanelHideSetActive(true);
+     }
+     
+     #endregion
+
+     #region Button Funcionality
+
+     private void TaskOnClick() {
+          if (MenuToActive != null) ActiveSubMenu();
+          else if (gameObject.name == "BackButton") BackManu();
+          else if (gameObject.name == "SaveButton") SaveButton();
+          else if (gameObject.name == "LoadButton") LoadButton();
+          else if (gameObject.name == "StartButton") StartButton();
+          else if (gameObject.name == "ExitButton") ExitButton();
+          else DisableSubMenus();
+     }
+
+     private void ActiveSubMenu() {
+          if (MenuToActive.transform.parent.name == "PanelRight") ActivePanelRight();
+          else {
+               gameController.MainMenuSetActive(false);
+               gameController.SubMenuSetActive(true);
+               if (gameObject.transform.parent.tag == "UIActivable") gameController.PushGameObject(gameObject.transform.parent.gameObject);
+          }
+
+          gameController.ActiveGameObject(MenuToActive);
+     }
+
+     private void ActiveSubMenu(GameObject obj) {
+          gameController.MainMenuSetActive(false);
+          gameController.SubMenuSetActive(true);
+          gameController.ActiveGameObject(obj);
+     }
+
+     private void BackManu() {    
+          if (gameController.StackCount() > 0) {
+               GameObject obj = gameController.PopGameObject();
+               ActiveSubMenu(obj);
+          } else {
+               DisablePanelRight();
+               DisableSubMenus();
+               gameController.SubMenuSetActive(false);
+               gameController.MainMenuSetActive(true);
+          }
+     }
+
+     private void SaveButton() {
+          Debug.Log("Save");
+     }
+
+     private void LoadButton() {
+          Debug.Log("Load");
+     }
+
+     private void StartButton() {
+          Debug.Log("Start");
+     }
+
+     private void ExitButton() {
+          Debug.Log("Leave");
+     }
+     
+     #endregion
+}
