f91347dcc6f22ccb52a938870c895ff021721801
deceiver91 <davidgv@openmailbox.org>
2017-04-26 11:20:17 +0200

    translate item

--- WorldEditor/Assets/Resources/Scripts/Menu/fixItem2.cs
+++ WorldEditor/Assets/Resources/Scripts/Menu/fixItem2.cs
@@ -9,6 +9,8 @@ public class fixItem2 : MonoBehaviour {
 
      private Button btn;
      private bool ReadyToFix = false;
+     private GameObject obj = null;
+     private string Layer = "";
 
      private void Start() {
           btn = GetComponent<Button>();
@@ -23,18 +25,16 @@ public class fixItem2 : MonoBehaviour {
 
      private void Update() {
 
+          if (ReadyToFix) ShowItem();
           if (Input.GetMouseButtonDown(1) && GlobalVariables.CanFixItem && ReadyToFix) RotateItem();
           if (Input.GetMouseButtonDown(0) && GlobalVariables.CanFixItem && ReadyToFix) InsertItem();
-
+          
      }
 
 
      private void InsertItem() {
-          RaycastHit hitInfo;
-          Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-          Physics.Raycast(ray, out hitInfo, Mathf.Infinity, LayerMask.GetMask("Terrain"));
-          Vector3 pos = new Vector3(hitInfo.point.x, hitInfo.point.y + YItemPosition, hitInfo.point.z);
-          GameObject obj = Instantiate(ItemToFix, pos, Quaternion.Euler(RotationItem)) as GameObject;
+
+          obj.layer = LayerMask.NameToLayer(Layer);
 
           if (CustomTag != "") obj.tag = CustomTag;
 
@@ -42,7 +42,8 @@ public class fixItem2 : MonoBehaviour {
           if (obj.tag == "Resources") GlobalVariables.ResourcesIsSelected = true;
           if (obj.tag == "Item") GlobalVariables.ItemsIsSelected = true;
           if (obj.tag == "NPCs") GlobalVariables.NPCsIsSelected = true;
-          
+
+          Debug.Log(string.Format("Obj.layer: {0}\tLayer: {1}", LayerMask.LayerToName(obj.layer), Layer));
 
           if (GlobalVariables.IsCtrlPressed) ReadyToFix = true;
           else {
@@ -59,8 +60,31 @@ public class fixItem2 : MonoBehaviour {
           else RotationItem.y += 90;
 
           if (RotationItem.y >= 360 || RotationItem.y <= -360) RotationItem.y = 0;
-          //Debug.Log(string.Format("Rotation: {0}", RotationItem));
+
+          obj.transform.rotation = Quaternion.Euler(RotationItem);
+
+          Debug.Log(string.Format("Rotation: {0}", RotationItem));
      }
      
+     private Vector3 RayCast() {
+          RaycastHit hitInfo;
+          Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+          if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity, LayerMask.GetMask("Terrain")))
+               return new Vector3(hitInfo.point.x, hitInfo.point.y + YItemPosition, hitInfo.point.z);
+          else return new Vector3(0, 0, 0);
+     }
+
+     private void ShowItem() {
+          
+          Vector3 pos = RayCast();
+          if (pos != new Vector3(0, 0, 0)) {
+               if (obj == null) {
+                    obj = Instantiate(ItemToFix, pos, Quaternion.Euler(RotationItem)) as GameObject;
+                    Layer = LayerMask.LayerToName(obj.layer);
+                    obj.layer = LayerMask.NameToLayer("Default");
+               }
+               obj.transform.position = pos;
+          }          
+     }
 
 }
